471a83ee09eb9f52fda752520ac3956302c3c6ff
zhuxinquan <1174272148@qq.com>
2017-03-25 11:16:50 +0800

    update

---
+++ SpringMVCHelloWorld/out/artifacts/SpringMVCHelloWorld_war_exploded/jsp/hello.jsp
@@ -0,0 +1,29 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhuxinquan
+  Date: 17-3-24
+  Time: 上午10:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>--%>
+<%--<%@taglib prefix="c" uri="http://www.springframework.org/tags" %>--%>
+<%@page isELIgnored="false" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+${message}
+hj
+</body>
+</html>
---
+++ SpringMVCHelloWorld/src/main/java/control/HelloWorldController.java
@@ -0,0 +1,23 @@
+package control;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * Created by zhuxinquan on 17-3-24.
+ */
+public class HelloWorldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
+        //1、收集参数、验证参数
+        //2、绑定参数到命令对象
+        //3、将命令对象传入业务对象进行业务处理
+        //4、选择下一个页面
+        ModelAndView mv = new ModelAndView();
+        //添加模型数据 可以是任意的POJO对象
+        mv.addObject("message", "Hello World!");
+        //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
+        mv.setViewName("/jsp/hello");
+        return mv;
+    }
+}
